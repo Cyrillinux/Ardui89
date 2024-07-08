05a02e88460c267e594ce1942dc45a2e62d7cae0
Cyril BARBATO <34169375+Cyrillinux@users.noreply.github.com>
2024-07-08 11:49:37 +0200

    Add files via upload

---
+++ Ardui89_demos/Hello/Hello.ino
@@ -0,0 +1,19 @@
+/*
+ * Hello
+ */
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+   // Serial port init
+  Serial89Init(); // Serial 89 init
+  Serial.begin(9600); // Monitor init (F_CPU = 11.0592Mhz)
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  Serial.println("Hello the world !");
+  delay(1000);                       // wait for a second
+}
