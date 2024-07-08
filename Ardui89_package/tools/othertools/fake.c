70dec6a5fea9c05eda23184ba189d7457d7eceee
Cyril BARBATO <34169375+Cyrillinux@users.noreply.github.com>
2024-07-08 11:55:26 +0200

    Add files via upload

---
+++ Ardui89_package/tools/othertools/fake.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    /* Debug
+    // See all args
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    };
+    return -1;
+     */
+    return 0;
+};
+
