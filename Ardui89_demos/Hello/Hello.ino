a60634d7bba3e40de916691a90beb7aa7820c4b5
Cyril BARBATO <34169375+Cyrillinux@users.noreply.github.com>
2024-07-15 21:52:32 +0200

    Update Hello.ino

--- Ardui89_demos/Hello/Hello.ino
+++ Ardui89_demos/Hello/Hello.ino
@@ -4,8 +4,8 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-   // Serial port init
-  Serial89Init(); // Serial 89 init
+  // Serial port init
+  // Serial89Init(); // Serial 89 init not necessary since V.0.1.0
   Serial.begin(9600); // Monitor init (F_CPU = 11.0592Mhz)
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
